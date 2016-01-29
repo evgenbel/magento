a71c5561b23a696a9a7651e94805d1be2c16918c
Evgeniy Belov <evgeniyblv@gmail.com>
2016-01-29 23:25:36 +0300

    Удаление товаров

--- app/code/local/Veeble/Giftregistry/Block/Item.php
+++ app/code/local/Veeble/Giftregistry/Block/Item.php
@@ -13,20 +13,11 @@ class Veeble_Giftregistry_Block_Item extends Mage_Core_Block_Template
         $customer = Mage::getSingleton('customer/session')->getCustomer();
         if ($customer) {
             $giftTable =  Mage::getSingleton('core/resource')->getTableName('veeble_giftregistry/item');
-            /*$collection = Mage::getModel('veeble_giftregistry/item')->getCollection()
-                ->addFieldToFilter('registry_id', $registry->getId());
 
-            $products = array();
-
-            foreach($collection as $item){
-                $products[] =$item->getProductId(); //Mage::getModel('catalog/product')->load($item->getProductId());
-            }*/
             $collection = Mage::getModel('catalog/product')->getCollection()
                 ->addAttributeToSelect(array('name', 'url', 'small_image', 'price', 'short_description'));
 
             $collection->getSelect()->join(array("g"=>$giftTable), "e.entity_id=g.product_id AND registry_id=".$registry->getId(), array("g.registry_id", "g.item_id"));
-            //$collection->addFieldToFilter('entity_id', $products);
-            //$collection->addFieldToFilter('registry_id', $registry->getId());
             return $collection;
         } else {
             return false;
